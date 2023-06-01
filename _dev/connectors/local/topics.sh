b4efdf867ec978d388d16daad24ece9ae5bcf3a2
S.Chukanov <svchukanov@sberbank.ru>
2023-06-01 11:50:15 +0300

    Вырезка названии схемы из названия топика. Задание нового префикса. Скрипт добавления топиков локально.

---
+++ _dev/connectors/local/topics.sh
@@ -0,0 +1,58 @@
+# connect-configs
+docker compose exec kafka \
+kafka-topics --create \
+--topic connect-configs \
+--bootstrap-server localhost:9092 \
+--replication-factor 1 \
+--partitions 1 \
+--config cleanup.policy=compact \
+--config compression.type=producer
+
+# connect-offsets
+docker compose exec kafka \
+kafka-topics --create \
+--topic connect-offsets \
+--bootstrap-server localhost:9092 \
+--replication-factor 1 \
+--partitions 25 \
+--config cleanup.policy=compact \
+--config compression.type=producer
+
+# connect-status
+docker compose exec kafka \
+kafka-topics --create \
+--topic connect-status \
+--bootstrap-server localhost:9092 \
+--replication-factor 1 \
+--partitions 5 \
+--config cleanup.policy=compact \
+--config compression.type=producer
+
+# __consumer_offsets
+docker compose exec kafka \
+kafka-topics --create \
+--topic __consumer_offsets \
+--bootstrap-server localhost:9092 \
+--replication-factor 1 \
+--partitions 5 \
+--config cleanup.policy=compact \
+--config compression.type=producer
+
+# Табличный топик
+# localhost.public.customers
+docker compose exec kafka \
+kafka-topics --create \
+--topic idpdb.customers \
+--bootstrap-server localhost:9092 \
+--replication-factor 1 \
+--partitions 1 \
+
+# Топик по транзакциям
+# localhost.transaction
+docker compose exec kafka \
+kafka-topics --create \
+--topic idpdb.transaction \
+--bootstrap-server localhost:9092 \
+--replication-factor 1 \
+--partitions 1 \
+
