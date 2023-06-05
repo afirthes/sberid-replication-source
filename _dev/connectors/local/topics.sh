2beeab68901b16fe887eeae8601d86a3127015a5
Molotkov-MS <MSMolotkov@sberbank.ru>
2023-06-05 16:27:44 +0300

    SID-24613

--- _dev/connectors/local/topics.sh
+++ _dev/connectors/local/topics.sh
@@ -1,7 +1,7 @@
 # connect-configs
 docker compose exec kafka \
 kafka-topics --create \
---topic connect-configs \
+--topic source.primary.connect-configs \
 --bootstrap-server localhost:9092 \
 --replication-factor 1 \
 --partitions 1 \
@@ -11,38 +11,38 @@ kafka-topics --create \
 # connect-offsets
 docker compose exec kafka \
 kafka-topics --create \
---topic connect-offsets \
+--topic source.primary.connect-offsets \
 --bootstrap-server localhost:9092 \
 --replication-factor 1 \
---partitions 25 \
+--partitions 1 \
 --config cleanup.policy=compact \
 --config compression.type=producer
 
 # connect-status
 docker compose exec kafka \
 kafka-topics --create \
---topic connect-status \
+--topic source.primary.connect-status \
 --bootstrap-server localhost:9092 \
 --replication-factor 1 \
---partitions 5 \
+--partitions 1 \
 --config cleanup.policy=compact \
 --config compression.type=producer
 
-# __consumer_offsets
-docker compose exec kafka \
-kafka-topics --create \
---topic __consumer_offsets \
---bootstrap-server localhost:9092 \
---replication-factor 1 \
---partitions 5 \
---config cleanup.policy=compact \
---config compression.type=producer
+## __consumer_offsets
+#docker compose exec kafka \
+#kafka-topics --create \
+#--topic __consumer_offsets \
+#--bootstrap-server localhost:9092 \
+#--replication-factor 1 \
+#--partitions 5 \
+#--config cleanup.policy=compact \
+#--config compression.type=producer
 
 # Табличный топик
 # localhost.public.customers
 docker compose exec kafka \
 kafka-topics --create \
---topic idpdb.customers \
+--topic idpdb.primary.customers \
 --bootstrap-server localhost:9092 \
 --replication-factor 1 \
 --partitions 1 \
@@ -51,7 +51,7 @@ kafka-topics --create \
 # localhost.transaction
 docker compose exec kafka \
 kafka-topics --create \
---topic idpdb.transaction \
+--topic idpdb.primary.transaction \
 --bootstrap-server localhost:9092 \
 --replication-factor 1 \
 --partitions 1 \
