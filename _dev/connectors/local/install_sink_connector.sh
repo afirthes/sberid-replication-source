e84b202ab0ab2d08b20d3049a2e3ed3d021acfb9
S.Chukanov <svchukanov@sberbank.ru>
2023-06-09 16:04:06 +0300

    Еще немного тестирования.

--- _dev/connectors/local/install_sink_connector.sh
+++ _dev/connectors/local/install_sink_connector.sh
@@ -19,21 +19,21 @@ curl -X PUT \
   http://localhost:28084/connectors/sink-connector-a/config
 
 printf "\nconnector table-a has been deployed"
-#
-##sleep 20
-#
-#curl -X PUT \
-#  -H 'Content-Type: application/json' \
-#  --data-binary "@jdbc-sink-connector-table-b.json" \
-#  http://localhost:28084/connectors/sink-connector-b/config
-#
-#printf "\nconnector table-b has been deployed"
-#
-##sleep 20
-#
-#curl -X PUT \
-#  -H 'Content-Type: application/json' \
-#  --data-binary "@jdbc-sink-connector-table-c.json" \
-#  http://localhost:28084/connectors/sink-connector-c/config
-#
-#printf "\nconnector table-c has been deployed"
+
+#sleep 20
+
+curl -X PUT \
+  -H 'Content-Type: application/json' \
+  --data-binary "@jdbc-sink-connector-table-b.json" \
+  http://localhost:28084/connectors/sink-connector-b/config
+
+printf "\nconnector table-b has been deployed"
+
+#sleep 20
+
+curl -X PUT \
+  -H 'Content-Type: application/json' \
+  --data-binary "@jdbc-sink-connector-table-c.json" \
+  http://localhost:28084/connectors/sink-connector-c/config
+
+printf "\nconnector table-c has been deployed"
--- _dev/connectors/local/sender/src/main/java/com/example/sender/SenderApplication.java
+++ _dev/connectors/local/sender/src/main/java/com/example/sender/SenderApplication.java
@@ -10,6 +10,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.random.RandomGenerator;
 
 @SpringBootApplication
@@ -26,68 +28,63 @@ public class SenderApplication {
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-//            ExecutorService ex = Executors.newFixedThreadPool(10);
+            ExecutorService ex = Executors.newFixedThreadPool(100);
 
-//            for(int x = 0; x < 1; x++) {
-//                ex.submit(() -> {
+            for (int x = 0; x < 100; x++) {
+                ex.submit(() -> {
 
-            for(int z=0; z<3; z++) {
+                    for (int z = 0; z < 3; z++) {
 
-                Random rand = new Random();
-                String correlation = UUID.randomUUID().toString().substring(0,10);
-                Map<String,Object> parameters = null;
+                        Random rand = new Random();
+                        String correlation = UUID.randomUUID().toString().substring(0, 10);
+                        Map<String, Object> parameters = null;
 
-                int c = RandomGenerator.getDefault().nextInt(1, 5);
-                List<Integer> a_ids = new ArrayList<>();
-                for(int i = 0; i < c; i++) {
-                    parameters = new HashMap<>();
-                    parameters.put("correlation", correlation);
-                    SimpleJdbcInsert insert = new SimpleJdbcInsert(template).withTableName("table_a").usingGeneratedKeyColumns("id");
-                    Integer a_id = insert.executeAndReturnKey(parameters).intValue();
-                    a_ids.add(a_id);
-                }
+                        int c = RandomGenerator.getDefault().nextInt(1, 5);
+                        List<Integer> a_ids = new ArrayList<>();
+                        for (int i = 0; i < c; i++) {
+                            parameters = new HashMap<>();
+                            parameters.put("correlation", correlation);
+                            SimpleJdbcInsert insert = new SimpleJdbcInsert(template).withTableName("table_a").usingGeneratedKeyColumns("id");
+                            Integer a_id = insert.executeAndReturnKey(parameters).intValue();
+                            a_ids.add(a_id);
+                        }
 
-//                c = RandomGenerator.getDefault().nextInt(1, 5);
-//                List<Integer> b_ids = new ArrayList<>();
-//                for(int i = 0; i < c; i++) {
-//                    parameters = new HashMap<>();
-//                    parameters.put("correlation", correlation);
-//                    parameters.put("a_id", a_ids.get(rand.nextInt(a_ids.size())));
-//                    SimpleJdbcInsert insert = new SimpleJdbcInsert(template).withTableName("table_b").usingGeneratedKeyColumns("id");
-//                    Integer b_id = insert.executeAndReturnKey(parameters).intValue();
-//                    b_ids.add(b_id);
-//                }
-//
-//                c = RandomGenerator.getDefault().nextInt(1, 5);
-//                List<Integer> c_ids = new ArrayList<>();
-//                for(int i = 0; i < c; i++) {
-//                    parameters = new HashMap<>();
-//                    parameters.put("correlation", correlation);
-//                    parameters.put("a_id", a_ids.get(rand.nextInt(a_ids.size())));
-//                    parameters.put("b_id", b_ids.get(rand.nextInt(b_ids.size())));
-//                    SimpleJdbcInsert insert = new SimpleJdbcInsert(template).withTableName("table_c").usingGeneratedKeyColumns("id");
-//                    Integer c_id = insert.executeAndReturnKey(parameters).intValue();
-//                    c_ids.add(c_id);
-//                }
-//
-//                System.out.println(a_ids);
-//                System.out.println(b_ids);
-//                System.out.println(c_ids);
+                        c = RandomGenerator.getDefault().nextInt(1, 5);
+                        List<Integer> b_ids = new ArrayList<>();
+                        for (int i = 0; i < c; i++) {
+                            parameters = new HashMap<>();
+                            parameters.put("correlation", correlation);
+                            parameters.put("a_id", a_ids.get(rand.nextInt(a_ids.size())));
+                            SimpleJdbcInsert insert = new SimpleJdbcInsert(template).withTableName("table_b").usingGeneratedKeyColumns("id");
+                            Integer b_id = insert.executeAndReturnKey(parameters).intValue();
+                            b_ids.add(b_id);
+                        }
+
+                        c = RandomGenerator.getDefault().nextInt(1, 5);
+                        List<Integer> c_ids = new ArrayList<>();
+                        for (int i = 0; i < c; i++) {
+                            parameters = new HashMap<>();
+                            parameters.put("correlation", correlation);
+                            parameters.put("a_id", a_ids.get(rand.nextInt(a_ids.size())));
+                            parameters.put("b_id", b_ids.get(rand.nextInt(b_ids.size())));
+                            SimpleJdbcInsert insert = new SimpleJdbcInsert(template).withTableName("table_c").usingGeneratedKeyColumns("id");
+                            Integer c_id = insert.executeAndReturnKey(parameters).intValue();
+                            c_ids.add(c_id);
+                        }
+
+                        System.out.println(a_ids);
+                        System.out.println(b_ids);
+                        System.out.println(c_ids);
+
+                    }
+
+                    // Deletion
+
+
+                });
+
 
             }
-
-            // Deletion
-
-
-
-
-
-
-
-//                });
-
-
-//            }
         };
     }
 
