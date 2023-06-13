a9adeefd5d665d1ed3f178350b0789cc4241c8ff
Molotkov-MS <MSMolotkov@sberbank.ru>
2023-06-13 13:54:14 +0300

    SID-24613 tombstones в триггере не учитывается удаление удалены лишние плагины дл kafka-connect образа

--- _dev/connectors/local/scripts/primary.sql
+++ _dev/connectors/local/scripts/primary.sql
@@ -55,7 +55,7 @@ BEGIN
     FOR tbl_name IN SELECT table_name FROM information_schema.tables WHERE table_schema = schema_name LOOP
             EXECUTE format('ALTER TABLE %I.%I ADD COLUMN IF NOT EXISTS change_source TEXT default ''PRIMARY'';', schema_name, tbl_name);
             EXECUTE format('DROP TRIGGER IF EXISTS trigger_change_source_%s on %I.%I', tbl_name, schema_name, tbl_name);
-            EXECUTE format('CREATE TRIGGER trigger_change_source_%s BEFORE INSERT OR UPDATE OR DELETE ON %I.%I FOR EACH ROW EXECUTE FUNCTION update_change_source()', tbl_name, schema_name, tbl_name);
+            EXECUTE format('CREATE TRIGGER trigger_change_source_%s BEFORE INSERT OR UPDATE ON %I.%I FOR EACH ROW EXECUTE FUNCTION update_change_source()', tbl_name, schema_name, tbl_name);
         END LOOP;
 END
 $$ LANGUAGE plpgsql;
